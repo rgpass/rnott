9f2b1e9a0c1db8a5ffee9203ad3308242c8ee9bf
Gerry Pass <rgpass@gmail.com>
2013-08-20 11:58:08 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rnott::Application.config.secret_token = '402493c476d750c63a8b2f9b6b6819099213559cb93b86d632ee7eae8511fa1b39d3f36da0be6856de2b7f2a477d9558ed2b6a8eb97a70b5606d85f5e670d09f'
