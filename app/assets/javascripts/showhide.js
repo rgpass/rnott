b45abd32bca6f9ad22d1070f85ab9e8a16b5e381
Gerry Pass <rgpass@gmail.com>
2013-08-28 20:27:49 -0400

    Fix jQuery toggle

--- app/assets/javascripts/showhide.js
+++ app/assets/javascripts/showhide.js
@@ -1,18 +1,42 @@
 $(document).ready(function(){
-	$(".dinnertoggle").hide();
-	$(".drinkstoggle").hide();
+	// $(".dinnertoggle").hide();
+	// $(".drinkstoggle").hide();
+
+	// if ($('#dinner').is(':checked')){
+	// 	$(".dinnertoggle").show();
+	// };
+	// if ($('#drinks').is(':checked')){
+	// 	$(".drinkstoggle").show();
+	// };
 
 	if ($('#dinner').is(':checked')){
-		$(".dinnertoggle").show();
-	};
-	if ($('#drinks').is(':checked')){
-		$(".drinkstoggle").show();
+		$('.dinnertoggle').removeClass('hidden');
 	};
 
-  $(".dinnerselect").click(function(){
-  	$(".dinnertoggle").toggle();
-  });
-  $(".drinksselect").click(function(){
-  	$(".drinkstoggle").toggle();
-  });
+	if ($('#drinks').is(':checked')){
+		$('.drinkstoggle').removeClass('hidden');
+	};
+
+  // $(".dinnerselect").click(function(){
+  // 	$(".dinnertoggle").toggle(1000);
+  // });
+  // $(".drinksselect").click(function(){
+  // 	$(".drinkstoggle").toggle(1000);
+  // });
+
+	$(".dinnerselect").click(function(){
+		$(".dinnertoggle").toggleClass('hidden');
+	});
+	$(".drinksselect").click(function(){
+		$(".drinkstoggle").toggleClass('hidden');
+	});
 });
+
+
+// $('thumbnail').toggle(function(){
+//     $('.play', this).removeClass('pausing');
+//     $('.play', this).addClass('playing');
+// }, function(){
+//     $('.play', this).addClass('pausing');
+//     $('.play', this).removeClass('playing');
+// });
