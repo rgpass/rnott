8d3722c42fc54d213ec6fb006c825cff7358d2bf
Gerry Pass <rgpass@gmail.com>
2013-08-30 17:46:30 -0400

    Add count for number of options and Format icons

--- app/assets/javascripts/showhide.js
+++ app/assets/javascripts/showhide.js
@@ -5,29 +5,53 @@ function goBack() {
 $(document).ready(function(){
 	if ($('#dinner').is(':checked')){
 		$('.dinnertoggle').removeClass('hidden');
-		$('path#dinnericon').attr('style','fill:grey');
+		$('path#dinnericon').attr('style','fill:white');
+		$('label.dinnerselect').addClass('selected');
 	};
 
 	if ($('#drinks').is(':checked')){
 		$('.drinkstoggle').removeClass('hidden');
-		$('path#drinksicon').attr('style','fill:grey');
+		$('path#drinksicon').attr('style','fill:white');
+		$('label.drinksselect').addClass('selected');
 	};
 
 	$(".dinnerselect").click(function(){
 		$(".dinnertoggle").toggle("fast", "swing");
 		if($('#dinner').is(':checked')){
-			$('path#dinnericon').attr('style','fill:black');
+			$('path#dinnericon').attr('style','fill:#2BA6CB');
 		} else {
-			$('path#dinnericon').attr('style','fill:grey');
+			$('path#dinnericon').attr('style','fill:white');
 		};
+		$('label.dinnerselect').toggleClass('selected');
 	});
 
 	$(".drinksselect").click(function(){
 		$(".drinkstoggle").toggle("fast", "swing");
 		if($('#drinks').is(':checked')){
-			$('path#drinksicon').attr('style','fill:black');
+			$('path#drinksicon').attr('style','fill:#2BA6CB');
 		} else {
-			$('path#drinksicon').attr('style','fill:grey');
+			$('path#drinksicon').attr('style','fill:white');
 		};
+		$('label.drinksselect').toggleClass('selected');
+	});
+
+	$(".dinnershow").click(function(){
+		$(".dinnername").toggle();
+		$("a.dinnershow").text(function(i, txt) {
+			return txt == "I'm lame and hate surprises." ? "Hide it!" : "I'm lame and hate surprises.";
+		});
+	});
+
+	$(".drinksshow").click(function(){
+		$(".drinksname").toggle();
+		$("a.drinksshow").text(function(i, txt) {
+			return txt == "I'm lame and hate surprises." ? "Hide it!" : "I'm lame and hate surprises.";
+		});
 	});
 });
+
+// $('[class^=link]').click(function() {
+//     $('span', this).text(function(i, txt) {
+//         return txt === "Open" ? "Close" : "Open";
+//     }).end().parent().next().toggle();
+// });
